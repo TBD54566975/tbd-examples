e4dfc42d6c79b9c99fcb70932010d88cc3eef9b8
finn <fherzfeld@tbd.email>
2024-06-05 13:04:38 -0700

    Add examples from developer.tbd.website repo

---
+++ javascript/shared-todo-completed/.nuxt/types/middleware.d.ts
@@ -0,0 +1,7 @@
+import type { NavigationGuard } from 'vue-router'
+export type MiddlewareKey = string
+declare module "../../../../../node_modules/.pnpm/nuxt@3.10.3_@types+node@18.16.19_rollup@2.61.1_sass@1.63.6_typescript@5.3.3_vite@5.1.4_xml2js@0.6.2/node_modules/nuxt/dist/pages/runtime/composables" {
+  interface PageMeta {
+    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
+  }
+}
---
+++ javascript/shared-todo-starter/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  telemetry: false,
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+  },
+  devServer: {
+    port: 5176
+  }
+})
