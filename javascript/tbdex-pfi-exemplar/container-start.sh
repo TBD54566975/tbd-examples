26f4218e581aa4bdc89ce455ce00af83bd06fb39
finn <fherzfeld@tbd.email>
2024-08-07 11:18:03 -0700

    update container entrypoint

---
+++ javascript/tbdex-pfi-exemplar/container-start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -euo pipefail
+export DATABASE_URL="postgres://${SEC_DB_USER}:${SEC_DB_PASSWORD}@${SEC_DB_HOST}:${SEC_DB_PORT}/${SEC_DB_NAME}"
+set -x
+export DBMATE_MIGRATIONS_DIR="./db/migrations"
+dbmate --wait --wait-timeout=60s up
+# npm run seed-offerings
+npm run server
