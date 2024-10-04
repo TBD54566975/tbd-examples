4cd7d2b7cd256fefc90b83208f50be718ece8801
leordev <leordev@gmail.com>
2024-10-04 16:39:53 -0400

    fix test

--- javascript/dwa-starter-react-vite/playwright.config.ts
+++ javascript/dwa-starter-react-vite/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:4173",
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -72,8 +72,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm preview",
-    url: "http://127.0.0.1:4173",
+    command: process.env.CI ? "vite preview --port 5173" : "vite dev",
+    url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
 });
