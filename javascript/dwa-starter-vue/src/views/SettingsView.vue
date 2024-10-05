968615c0e75a8aa34aaecf091fae7814d8d644b1
johnny evans <evanseburu32@gmail.com>
2024-10-05 02:55:24 +0100

    pwa assets generation and settings page form

--- javascript/dwa-starter-vue/src/views/SettingsView.vue
+++ javascript/dwa-starter-vue/src/views/SettingsView.vue
@@ -3,7 +3,7 @@ import { ReloadIcon } from '@radix-icons/vue'
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import * as z from 'zod'
-
+import { PersonIcon } from '@radix-icons/vue'
 import { Button } from '@/components/ui/button'
 import {
   FormControl,
@@ -23,7 +23,25 @@ const formSchema = toTypedSchema(
   })
 )
 
-const name = ref('John Doe')
+const name = ref('')
+const profileImage = ref('')
+const fileInputKey = ref(0)
+
+const handleImageUpload = (event: Event) => {
+  const file = (event.target as HTMLInputElement).files?.[0]
+  if (file) {
+    const reader = new FileReader()
+    reader.onload = () => {
+      profileImage.value = reader.result as string
+    }
+    reader.readAsDataURL(file)
+  }
+}
+
+const clearImage = () => {
+  profileImage.value = ''
+  fileInputKey.value++
+}
 
 const { isFieldDirty, handleSubmit, isSubmitting } = useForm({
   validationSchema: formSchema
@@ -42,16 +60,45 @@ const onSubmit = handleSubmit((values) => {
   <div class="flex flex-col gap-4">
     <h1>Settings</h1>
     <form class="lg:w-1/3 w-full space-y-6" @submit.prevent="onSubmit">
+      <img
+        v-if="profileImage"
+        :src="profileImage"
+        alt="Profile Preview"
+        class="w-24 h-24 rounded-xl"
+      />
+      <PersonIcon v-else class="w-24 h-24" />
+
+      <FormField name="profileImage" class="w-full">
+        <FormItem>
+          <FormLabel>Profile Image</FormLabel>
+          <FormControl>
+            <div class="flex items-center gap-2">
+              <Input
+                type="file"
+                accept="image/*"
+                class="dark:bg-gray-700"
+                @change="handleImageUpload"
+                :key="fileInputKey"
+              />
+              <Button type="button" @click="clearImage" v-if="profileImage"> Clear Image </Button>
+            </div>
+          </FormControl>
+          <FormDescription>Upload your profile image.</FormDescription>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
       <FormField v-model="name" name="name" :validate-on-blur="!isFieldDirty">
         <FormItem>
           <FormLabel>Name</FormLabel>
           <FormControl>
             <Input type="text" placeholder="Enter your name" v-model="name" />
           </FormControl>
-          <FormDescription> This is your public display name. </FormDescription>
+          <FormDescription>This is your public display name.</FormDescription>
           <FormMessage />
         </FormItem>
       </FormField>
+
       <Button type="submit" :disabled="isSubmitting">
         <ReloadIcon v-if="isSubmitting" class="w-4 h-4 mr-2 animate-spin" /> Save
       </Button>
