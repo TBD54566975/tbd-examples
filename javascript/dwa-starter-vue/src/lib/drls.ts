da8a0fcb3f53376f0b647e706e3f5a2887e7ac8e
johnny evans <evanseburu32@gmail.com>
2024-10-16 18:13:43 +0100

    test fix

--- javascript/dwa-starter-vue/src/lib/drls.ts
+++ javascript/dwa-starter-vue/src/lib/drls.ts
@@ -1,42 +1,35 @@
+import { Convert } from '@web5/common'
+
 export const drlFetchRecord = async (did: string, recordId: string) => {
-  const dwebUrl = `https://dweb/${did}/records/${recordId}`;
-  return fetch(dwebUrl);
-};
+  const dwebUrl = `https://dweb/${did}/records/${recordId}`
+  return fetch(dwebUrl)
+}
 
 export const drlFetchRecordJson = async (did: string, recordId: string) => {
-  const res = await drlFetchRecord(did, recordId);
-  return handleJsonResponse(res);
-};
+  const res = await drlFetchRecord(did, recordId)
+  return handleJsonResponse(res)
+}
 
 export const drlReadProtocolUrl = (did: string, protocolIdUri: string, subpath: string) => {
-  const encodedProtocolIdUri = encodeURIComponent(`${protocolIdUri}`);
-  return `https://dweb/${did}/read/protocols/${encodedProtocolIdUri}/${subpath}`;
-};
+  const encodedProtocolIdUri = Convert.string(protocolIdUri).toBase64Url()
+  return `https://dweb/${did}/read/protocols/${encodedProtocolIdUri}/${subpath}`
+}
 
-export const drlReadProtocol = async (
-  did: string,
-  protocolIdUri: string,
-  subpath: string
-) => {
-  const dwebUrl = drlReadProtocolUrl(did, protocolIdUri, subpath);
-  return fetch(dwebUrl);
-};
+export const drlReadProtocol = async (did: string, protocolIdUri: string, subpath: string) => {
+  const dwebUrl = drlReadProtocolUrl(did, protocolIdUri, subpath)
+  return fetch(dwebUrl)
+}
 
-export const drlReadProtocolJson = async (
-  did: string,
-  protocolIdUri: string,
-  subpath: string
-) => {
-  const res = await drlReadProtocol(did, protocolIdUri, subpath);
-  console.info("drlReadProtocolJson", {res}); // TODO: remove
-  return handleJsonResponse(res);
-};
+export const drlReadProtocolJson = async (did: string, protocolIdUri: string, subpath: string) => {
+  const res = await drlReadProtocol(did, protocolIdUri, subpath)
+  return handleJsonResponse(res)
+}
 
 const handleJsonResponse = async (res: Response) => {
   if (res.ok) {
-    return res.json();
+    return res.json()
   } else {
-    const errorText = await res.text().catch((err) => err);
-    throw new Error(`${res.status} ${errorText}`);
+    const errorText = await res.text().catch((err) => err)
+    throw new Error(`${res.status} ${errorText}`)
   }
-};
+}
