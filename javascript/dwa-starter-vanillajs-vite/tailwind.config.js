bc9da6b0eb7af8bb1bfc744648b064491a500710
deepanshu tyagi <123951725+29deepanshutyagi@users.noreply.github.com>
2024-10-18 03:51:50 +0530

    Implement theme toggle functionality and update styles for light/dark modes (#320)

--- javascript/dwa-starter-vanillajs-vite/tailwind.config.js
+++ javascript/dwa-starter-vanillajs-vite/tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Enable class-based dark mode
   content: [
-    "./index.html",
-    "./components.js",
-
-   
+    './index.html',
+    './components.js',
+  ], safelist: [
+    'light-mode',
+    'dark-mode',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBg: '#1a1a1a',
+        lightBg: '#ffffff',
+        darkText: '#f5f5f5',
+        lightText: '#000000',
+      },
+    },
   },
   plugins: [],
-}
+};
